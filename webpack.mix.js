1f4bf1287f2c26fb1011232606682504c2364ce3
Eduard Kizmann <ek@stilwaechter.de>
2018-07-03 16:36:45 +0200

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,33 @@
+/**
+ * Webpack / Mix configuration file
+ */
+
+let mix = require('laravel-mix');
+let path = require('path');
+
+/**
+ * Do default configuration
+ */
+
+mix.options({
+    clearConsole: true, processCssUrls: false
+});
+
+/**
+ * Set public and resource path to root
+ */
+
+mix.setPublicPath(
+    path.resolve(__dirname)
+);
+
+mix.setResourceRoot(
+    path.resolve(__dirname)
+);
+
+/**
+ * Compile files
+ */
+
+mix.sass('src/scss/demo.scss', 'dist/css/demo.css');
+mix.sass('src/scss/bootstrap.scss', 'dist/css/grid.css');
